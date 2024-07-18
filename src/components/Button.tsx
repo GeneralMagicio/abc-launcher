c06c3595e13f6b80e954dfa849ab12224951e6a1
Cherik <Pourcheriki@gmail.com>
2024-07-18 13:22:02 +0330

    set button text size

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ interface IButtonProps {
 
 export const Button: FC<IButtonProps> = ({ children }) => {
   return (
-    <button className="inline-block text-white font-bold border-none rounded-full bg-pink-500 py-4 px-10">
+    <button className="inline-block text-white font-bold text-xs border-none rounded-full bg-pink-500 py-4 px-10">
       {children}
     </button>
   );
