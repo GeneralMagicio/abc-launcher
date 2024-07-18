78c654515321cf484bb574fa1720e51e81406363
Cherik <Pourcheriki@gmail.com>
2024-07-18 12:11:26 +0330

    add wagmi config

---
+++ src/config/wagmi.ts
@@ -0,0 +1,27 @@
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+
+import { cookieStorage, createStorage } from "wagmi";
+import { mainnet, sepolia } from "wagmi/chains";
+
+// Your WalletConnect Cloud project ID
+export const projectId = "cea85f2edebb693e0443973f37e23153";
+
+// Create a metadata object
+const metadata = {
+  name: "qacc",
+  description: "AppKit Example",
+  url: "https://web3modal.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia] as const;
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+});
