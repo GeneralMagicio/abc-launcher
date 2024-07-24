354004324e2f2a6973dda9dde99b22df69248c5f
Cherik <Pourcheriki@gmail.com>
2024-07-24 10:28:23 +0330

    update button to get button props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,15 @@
-import { type FC } from "react";
+import { FC, ButtonHTMLAttributes } from "react";
 
-interface IButtonProps {
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export const Button: FC<IButtonProps> = ({ children }) => {
+export const Button: FC<IButtonProps> = ({ children, ...props }) => {
   return (
-    <button className="inline-block text-white font-bold text-xs border-none rounded-full bg-pink-500 py-4 px-10">
+    <button
+      {...props}
+      className="inline-block text-white font-bold text-xs border-none rounded-full bg-pink-500 py-4 px-10"
+    >
       {children}
     </button>
   );
