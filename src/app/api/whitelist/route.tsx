dd34285cdf3909ba89d502af0ec15492e44284d6
Cherik <Pourcheriki@gmail.com>
2024-07-24 18:52:11 +0330

    add random address to whitelist

--- src/app/api/whitelist/route.tsx
+++ src/app/api/whitelist/route.tsx
@@ -7,9 +7,18 @@ export async function POST(request: Request) {
   // Perform your logic here, e.g., save the address to a database or process it as needed
   console.log("Received address:", address);
 
+  let isWhiteListed = false;
+  // Check if the address is whitelisted
+  if (
+    address.toLowerCase() ===
+    "0x8F48094a12c8F99d616AE8F3305D5eC73cBAA6b6".toLowerCase()
+  ) {
+    isWhiteListed = true;
+  }
+
   // For this example, we'll just return the received address
   return NextResponse.json({
-    isWhiteListed: true,
+    isWhiteListed,
     message: "Address received",
     address,
   });
