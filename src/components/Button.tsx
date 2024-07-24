b38fafe5e31fc0dbfd05913968acc19cf85f5bee
Cherik <Pourcheriki@gmail.com>
2024-07-24 19:14:17 +0330

    add loading to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,18 @@
 import { FC, ButtonHTMLAttributes } from "react";
+import { Spinner } from "./Loading/Spinner";
 
 interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  loading?: boolean;
 }
 
-export const Button: FC<IButtonProps> = ({ children, ...props }) => {
+export const Button: FC<IButtonProps> = ({ children, loading, ...props }) => {
   return (
     <button
       {...props}
-      className="inline-block text-white font-bold text-xs border-none rounded-full bg-pink-500 py-4 px-10"
+      className="inline-block text-white font-bold text-xs border-none rounded-full bg-pink-500 py-4 px-10 flex gap-2"
     >
+      {loading && <Spinner size={16} />}
       {children}
     </button>
   );
