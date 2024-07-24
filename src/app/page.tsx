c905e2a9718de2433d0189a7eb95fb00e45813ee
Cherik <Pourcheriki@gmail.com>
2024-07-24 18:52:29 +0330

    check the user address and redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,45 @@
+"use client";
+
 import { Button } from "@/components/Button";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
 
 export default function Home() {
+  const { open: openWeb3Modal } = useWeb3Modal();
+  const { address } = useAccount();
+  const router = useRouter();
+
+  const handleLaunchToken = async () => {
+    if (!address) {
+      await openWeb3Modal();
+    }
+    if (address) {
+      console.log("Launching Token for address:", address);
+      // Send the address to your endpoint
+      fetch("/api/whitelist", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ address }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success:", data);
+          if (data.isWhiteListed) {
+            router.push("/token-form");
+          } else {
+            router.push("/not-whitelisted");
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+  };
+
   return (
     <main className="container text-center flex flex-col items-center gap-8">
       <div className="flex flex-col items-center gap-6 py-10">
@@ -23,7 +61,7 @@ export default function Home() {
           height={32}
         />
       </div>
-      <Button>Launch Token</Button>
+      <Button onClick={handleLaunchToken}>Launch Token</Button>
     </main>
   );
 }
