21bf8ee29f866bbd3dad664dde1657ae7d1fc280
Cherik <Pourcheriki@gmail.com>
2024-07-25 17:20:36 +0330

    add grant row

--- src/components/TokenForm/ConfirmStep.tsx
+++ src/components/TokenForm/ConfirmStep.tsx
@@ -51,6 +51,17 @@ const ConfirmStep: React.FC<{ onNext: () => void; onBack: () => void }> = ({
               Please review everything before Launch
             </h2>
             <div className="grid grid-cols-[auto_24px_auto] gap-y-4 gap-x-6 px-5 py-4 border-t-[1px] border-b-[1px]">
+              <p className="text-lg text-gray-600">Grant size</p>
+              <Image
+                src="/images/icons/arrow-right.svg"
+                alt="logo"
+                width={24}
+                height={24}
+              />
+              <div className="border-2 rounded-md border-success-600 bg-success-100 text-success-700 flex items-start gap-4 px-4 py-1">
+                <p className="text-lg">65,000 POL</p>
+                <p className="text-xs">$50,000</p>
+              </div>
               {info.map((item) => (
                 <Fragment key={item.label}>
                   <p className="text-lg text-gray-600">{item.label}</p>
