42abaeaac9b7e302a67f09921204d9d876e30bd2
Cherik <Pourcheriki@gmail.com>
2024-07-27 14:22:39 +0330

    add ipfs as know host to nextjs image

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", port: "", hostname: "ipfs.io" }],
+  },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
