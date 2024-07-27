4a9fff8279817d40f113b8c40312d8351ec24d63
Cherik <Pourcheriki@gmail.com>
2024-07-27 14:10:32 +0330

    add graphql to GRAPHQL_ENDPOINT

--- src/config/development.ts
+++ src/config/development.ts
@@ -4,7 +4,7 @@ const config = {
   SUPPORTED_CHAINS: [polygonZkEvmCardona] as const,
   GRAPHQL_ENDPOINT:
     process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ||
-    "https://impact-graph.serve.giveth.io",
+    "https://impact-graph.serve.giveth.io/graphql",
 };
 
 export default config;
--- src/config/production.ts
+++ src/config/production.ts
@@ -4,7 +4,7 @@ const config = {
   SUPPORTED_CHAINS: [polygonZkEvm] as const,
   GRAPHQL_ENDPOINT:
     process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ||
-    "https://mainnet.serve.giveth.io",
+    "https://mainnet.serve.giveth.io/graphql",
 };
 
 export default config;
