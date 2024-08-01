e64269acde08710d71dc82f31857f78b70f4a51e
Cherik <Pourcheriki@gmail.com>
2024-08-01 16:15:37 +0330

    rename route to ipfs

--- src/app/api/ipfs/route.tsx
+++ src/app/api/ipfs/route.tsx
@@ -26,3 +26,23 @@ export async function POST(request: Request) {
     );
   }
 }
+
+export async function DELETE(request: Request) {
+  const { ipfsHash } = await request.json();
+
+  try {
+    await axios.delete(`https://api.pinata.cloud/pinning/unpin/${ipfsHash}`, {
+      headers: {
+        pinata_api_key: process.env.PINATA_API_KEY,
+        pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY,
+      },
+    });
+
+    return NextResponse.json({ message: "File unpinned from IPFS" });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error unpinning file from IPFS" },
+      { status: 500 }
+    );
+  }
+}
