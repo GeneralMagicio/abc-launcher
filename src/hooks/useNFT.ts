0263a3bfc170f9035aab8a337c9c22a2c2917f44
Amin Latifi <a.latifi.al@gmail.com>
2024-08-21 18:16:20 +0330

    Update NFT contract

--- src/hooks/useNFT.ts
+++ src/hooks/useNFT.ts
@@ -1,11 +1,13 @@
 import { abi, bytecode } from "@/lib/nft";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
-import { usePublicClient, useWalletClient } from "wagmi";
+import { useAccount, usePublicClient, useWalletClient } from "wagmi";
 
 export const useNFT = () => {
   const walletClient = useWalletClient();
   const publicClient = usePublicClient();
+  const { address } = useAccount();
+  const DEFAULT_BASE_URI = "";
 
   const deploy = useMutation({
     mutationFn: async ({ name, symbol }: { name: string; symbol: string }) => {
@@ -15,7 +17,7 @@ export const useNFT = () => {
       const hash = await walletClient.data.deployContract({
         abi,
         bytecode,
-        args: [name, symbol],
+        args: [name, symbol, address, DEFAULT_BASE_URI],
       });
 
       toast.info(`Waiting for NFT deploy confirmation: ${hash}`);
