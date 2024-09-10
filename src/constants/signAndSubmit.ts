fcf5cc4454d4046c8d753b38f4e74b8dbe7acfca
kkatusic <kkatusic@gmail.com>
2024-09-10 14:28:56 +0200

    fix assign variables

--- src/constants/signAndSubmit.ts
+++ src/constants/signAndSubmit.ts
@@ -1,9 +1,9 @@
 import { TERMS_AND_CONDITIONS_HTML } from "./termsText";
 import { POLICY_STATEMENT_HTML } from "./privacyText";
 
-export const TERMS_AND_CONDITIONS = TERMS_AND_CONDITIONS_TEXT;
+export const TERMS_AND_CONDITIONS = TERMS_AND_CONDITIONS_HTML;
 
-export const POLICY_STATEMENT = POLICY_STATEMENT_TEXT;
+export const POLICY_STATEMENT = POLICY_STATEMENT_HTML;
 
 export enum MessageType {
   TermsAndConditions = "Terms&Conditions",
