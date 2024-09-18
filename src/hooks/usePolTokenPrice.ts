8ecc042f385e70f81c3e993f3ce72004012190eb
Amin Latifi <a.latifi.al@gmail.com>
2024-09-18 17:07:01 +0330

    Updated collateram amount and value in confirm page

---
+++ src/hooks/usePolTokenPrice.ts
@@ -0,0 +1,12 @@
+import { fetchPolTokenPrice } from "@/helpers/currency";
+import { useQuery } from "@tanstack/react-query";
+
+export const usePolTokenPrice = () => {
+  return useQuery({
+    queryKey: ["polTokenPrice"],
+    queryFn: fetchPolTokenPrice,
+    refetchInterval: 60_000, // 1 minute
+    refetchOnWindowFocus: false,
+    staleTime: 5 * 60_000, // 5 minutes
+  });
+};
