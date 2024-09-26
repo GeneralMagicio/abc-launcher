60b9e2d909c7fdb7bf8ac91c28db248c573ea8f1
kkatusic <kkatusic@gmail.com>
2024-09-26 17:14:03 +0200

    fixing input width again

--- src/components/TokenForm/TokenInfoStep.tsx
+++ src/components/TokenForm/TokenInfoStep.tsx
@@ -89,7 +89,6 @@ const TokenInfoStep: React.FC<{ onNext: () => void; onBack: () => void }> = ({
                 return isAddress(value) ? true : "Address in not valid"; // Add your validation logic here
               },
             }}
-            className="pr-1"
           />
 
           <Checkbox
