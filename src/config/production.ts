ea6dda8dd6495d73fcef722432c1ce6be7db6f57
Griff Green <saveoursonics@gmail.com>
2024-10-28 10:26:06 +0100

    Update production.ts

--- src/config/production.ts
+++ src/config/production.ts
@@ -5,7 +5,7 @@ import { Chain, polygonZkEvm } from "wagmi/chains";
 const config: EnvConfig = {
   SUPPORTED_CHAINS: [polygonZkEvm] as [Chain],
   COLATERAL_TOKEN: "0x22B21BedDef74FE62F031D2c5c8F7a9F8a4b304D" as Address,
-  INITIAL_ADMIN: "0x8DDF607FcFb260798Ae450cfc15292a75B4D4850",
+  INITIAL_ADMIN: "0x44a0eBe19A93C801AD97F308672fdc7B110B2128",
   GRAPHQL_ENDPOINT:
     process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ||
     "https://mainnet.serve.giveth.io/graphql",
@@ -20,12 +20,12 @@ const config: EnvConfig = {
     sellIsOpen: true,
     initialIssuanceSupply: "6400000", // human readable format
     // This will be deposited into the contract
-    initialCollateralSupply: "140964.2", // human readable format,
+    initialCollateralSupply: "152135", // human readable format,
   },
   SCAN_URL: "https://zkevm.polygonscan.com/",
   tokenIssueMaxSupply:
     "115792089237316195423570985008687907853269984665640564039457.584007913129639935",
-  gotToDashboardLink: "https://q-acc.vercel.app/create",
+  gotToDashboardLink: "https://q-acc.giveth.io/create",
 };
 
 export default config;
